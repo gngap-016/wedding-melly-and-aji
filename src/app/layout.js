d8cfb2c38476cf2173d92c014e32b0680683568f
gngap-016 <gilangajipanutan16@gmail.com>
2024-11-20 21:52:56 +0700

    Update meta data

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,27 +4,27 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Lamidi & Iba Lestari',
-  description: 'Undangan Pernikahan Lamidi, S.Pd. dan Iba Lestari, A.Md.Keb.',
+  title: 'M. Aji Ramadhan & Melly Angreini',
+  description: 'Undangan Pernikahan M. Aji Ramadhan dan Melly Angreini.',
   openGraph: {
-    title: 'Lamidi & Iba Lestari',
-    description: 'Undangan Pernikahan Lamidi, S.Pd. dan Iba Lestari, A.Md.Keb.',
+    title: 'M. Aji Ramadhan & Melly Angreini',
+    description: 'Undangan Pernikahan M. Aji Ramadhan dan Melly Angreini.',
     // url: process.env.HOST_URL,
     url: 'https://wedding-lamidi-dan-iba-lestari.vercel.app/',
-    siteName: 'Undangan Pernikahan Lamidi, S.Pd. dan Iba Lestari, A.Md.Keb.',
+    siteName: 'Undangan Pernikahan M. Aji Ramadhan dan Melly Angreini.',
     images: [
       {
         // url: `${process.env.HOST_URL}images/couple1.jpg`,
-        url: `https://wedding-lamidi-dan-iba-lestari.vercel.app/images/couple1.jpg`,
+        url: `https://wedding-lamidi-dan-iba-lestari.vercel.app/images/couple-1.jpg`,
         width: 800,
         height: 600,
       },
       {
         // url: `${process.env.HOST_URL}images/couple1.jpg`,
-        url: `https://wedding-lamidi-dan-iba-lestari.vercel.app/images/couple1.jpg`,
+        url: `https://wedding-lamidi-dan-iba-lestari.vercel.app/images/couple-1.jpg`,
         width: 1800,
         height: 1600,
-        alt: 'Undangan Pernikahan Lamidi & Iba Lestari',
+        alt: 'Undangan Pernikahan M. Aji Ramadhan & Melly Angreini',
       },
     ],
   },
